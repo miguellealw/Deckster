3912cea6b067cefb1fd7c00583da6d2aa0de1572
Miguel <miguellealwebdev@gmail.com>
2019-03-12 02:52:35 -0500

    Added container to explore and decks pages

--- src/screens/App/screens/Explore/index.js
+++ src/screens/App/screens/Explore/index.js
@@ -1,14 +1,27 @@
 import React from "react";
 import MediaQuery from "react-responsive";
+import styled from "styled-components";
 
 import SideNavigation from "shared/components/SideNavigation";
 import Navigation from "shared/components/Navigation";
 
+export const ExploreContainer = styled.div`
+  display: grid;
+  grid-template-rows: 1fr auto;
+
+  /* 500px or higher */
+  @media (min-width: 500px) {
+    grid-template-columns: 1fr 5fr;
+  }
+`;
+
 const Explore = () => {
   return (
-    <MediaQuery query={"(min-width: 500px)"}>
-      {matches => (matches ? <SideNavigation /> : <Navigation />)}
-    </MediaQuery>
+    <ExploreContainer>
+      <MediaQuery query={"(min-width: 500px)"}>
+        {matches => (matches ? <SideNavigation /> : <Navigation />)}
+      </MediaQuery>
+    </ExploreContainer>
   );
 };
 
