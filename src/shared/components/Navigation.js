abcf44daba176861ad2b15c18fa3e72636a57dbb
Miguel <miguellealwebdev@gmail.com>
2019-03-12 02:51:51 -0500

    Added dropdown to nav on smaller screens

--- src/shared/components/Navigation.js
+++ src/shared/components/Navigation.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@reach/router";
 import styled from "styled-components";
+import MediaQuery from "react-responsive";
 
 import Logo from "shared/components/Logo";
 
-const NavigationContatiner = styled.nav`
+const NavigationContainer = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -27,7 +28,7 @@ const ulStyles = {
   margin: 0
 };
 
-const LoginButton = styled.button`
+const NavButton = styled.button`
   margin: 0;
   font-family: Nunito;
   font-weight: bold;
@@ -44,19 +45,115 @@ const LoginButton = styled.button`
   }
 `;
 
+const ButtonContainer = styled.ul`
+  /* background: red; */
+  li {
+    display: inline-block;
+    margin-left: 1rem;
+  }
+`;
+
+const NavIcon = styled.div`
+  width: 1.3rem;
+  height: 0.4rem;
+
+  /* background: red; */
+
+  position: relative;
+
+  cursor: pointer;
+
+  &::after,
+  &::before {
+    content: "";
+    width: 100%;
+    height: 0.1rem;
+    background: white;
+  }
+
+  &::after {
+    position: absolute;
+    top: 0;
+  }
+
+  &::before {
+    position: absolute;
+    bottom: 0;
+  }
+`;
+
+const DropdownContainer = styled.ul`
+  font-size: 0.8rem;
+  min-width: 10rem;
+  min-height: 3rem;
+  background-color: #152134;
+  /* background: purple; */
+
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.3);
+
+  position: absolute;
+  top: 100%;
+  right: 0;
+
+  li {
+    color: white;
+    text-align: center;
+    padding: 1rem;
+    border-top: 1px solid #1a2941;
+    /* border-bottom: 1px solid #20324e; */
+
+    &:hover {
+      background: linear-gradient(to bottom, #009dff 0%, #0063ff 100%);
+    }
+  }
+`;
+
+const Dropdown = () => (
+  <DropdownContainer>
+    <Link to="/profile/my-decks">
+      <li>Profile</li>
+    </Link>
+    <Link to="/login">
+      <li>Login</li>
+    </Link>
+    <Link to="/signup">
+      <li>Signup</li>
+    </Link>
+  </DropdownContainer>
+);
+
 const Navigation = () => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <NavigationContatiner>
+    <NavigationContainer>
       <Logo />
-      <ul style={ulStyles}>
-        <li>
-          <Link to="/login">
-            <LoginButton>Login</LoginButton>
-          </Link>
-        </li>
-        {/* <li><button style={loginButtonStyles}>Sign Up</button></li> */}
-      </ul>
-    </NavigationContatiner>
+      <ButtonContainer style={ulStyles}>
+        <MediaQuery minWidth="500px">
+          <li>
+            <Link to="/profile/my-decks">
+              <NavButton>Profile</NavButton>
+            </Link>
+          </li>
+          <li>
+            <Link to="/login">
+              <NavButton>Login</NavButton>
+            </Link>
+          </li>
+          <li>
+            <Link to="/signup">
+              <NavButton>Sign Up</NavButton>
+            </Link>
+          </li>
+        </MediaQuery>
+        <MediaQuery maxWidth="500px">
+          <li>
+            <NavIcon aria-label="menu" onClick={() => setOpen(!isOpen)} />
+          </li>
+        </MediaQuery>
+      </ButtonContainer>
+      <MediaQuery maxWidth="500px">{isOpen && <Dropdown />}</MediaQuery>
+    </NavigationContainer>
   );
 };
 
