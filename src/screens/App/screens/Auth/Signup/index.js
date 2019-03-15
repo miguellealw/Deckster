9ffbbf1ce4d93092126865bba72ca51bc59451b3
Miguel <miguellealwebdev@gmail.com>
2019-03-15 02:06:05 -0500

    Removed unecessary imports

--- src/screens/App/screens/Auth/Signup/index.js
+++ src/screens/App/screens/Auth/Signup/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import { Link } from "@reach/router";
 
 import Input from "shared/components/Input";
--- src/screens/App/screens/Home/index.js
+++ src/screens/App/screens/Home/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import { Link } from "@reach/router";
 
 import Navigation from "shared/components/Navigation";
