7a5b58d1db6734683e4168f30a9c36a9d3fdd25d
Miguel <miguellealwebdev@gmail.com>
2019-10-17 00:25:33 -0500

    Created custom renderer for RTL

--- src/tests/Deck.test.js
+++ src/tests/Deck.test.js
@@ -1,33 +1,22 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+
+// RTL
+import { render, cleanup } from "utils/test-utils";
 import "@testing-library/jest-dom/extend-expect";
 
-// import { render, unmountComponentAtNode } from "react-dom";
-// import { create } from "react-test-renderer";
-// import { act } from "react-dom/test-utils";
-// import pretty from "pretty";
-
-import { ThemeProvider } from "styled-components";
-
-import theme from "../shared/theme";
+// Component
 import Deck from "../shared/components/Deck";
 
 afterEach(cleanup);
 
-const renderWithTheme = () => {
-  return render(
-    <ThemeProvider theme={theme}>
-      <Deck
-        title="test title"
-        firstCardName="first card test"
-        tags={["tag 1", "tag 2"]}
-      />
-    </ThemeProvider>
-  );
-};
-
 it("Renders with Deck Title", () => {
-  const { getByText } = renderWithTheme();
+  const { getByText } = render(
+    <Deck
+      title="test title"
+      firstCardName="first card test"
+      tags={["tag 1", "tag 2"]}
+    />
+  );
 
   // const deckTitle = getByText("test title").textContent;
   // expect(deckTitle).toBe("test title");
@@ -38,14 +27,26 @@ it("Renders with Deck Title", () => {
 it("Renders with Tags", () => {
   const fakeTags = ["tag 1", "tag 2"];
 
-  const { getAllByTestId } = renderWithTheme();
+  const { getAllByTestId } = render(
+    <Deck
+      title="test title"
+      firstCardName="first card test"
+      tags={["tag 1", "tag 2"]}
+    />
+  );
 
   const componentTags = getAllByTestId("tag").map(li => li.textContent);
   expect(componentTags).toEqual(fakeTags);
 });
 
 it("Renders with First Card Name", () => {
-  const { getByText } = renderWithTheme();
+  const { getByText } = render(
+    <Deck
+      title="test title"
+      firstCardName="first card test"
+      tags={["tag 1", "tag 2"]}
+    />
+  );
 
   const firstCardName = getByText("first card test").textContent;
   expect(firstCardName).toBe("first card test");
