954bda8326365cd06936757eca58e884ffbfc169
Miguel <miguellealwebdev@gmail.com>
2019-10-16 23:14:21 -0500

    Added footer

---
+++ src/shared/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const footerStyles = {
+  width: "100%",
+  padding: "3em",
+  // marginTop: "1em",
+  textAlign: "center",
+  // background: "red",
+};
+
+const Footer = () => <footer style={footerStyles}>&copy; Deckster</footer>;
+
+export default Footer;
--- src/shared/components/Layout.js
+++ src/shared/components/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Navigation from "shared/components/Navigation";
+import Footer from "shared/components/Footer";
 
 const Layout = props => (
   <div>
     <Navigation />
     {props.children}
+    <Footer />
   </div>
 );
 
