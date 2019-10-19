8583c205d997bae677850e915c07cf15d8e60e32
Miguel <miguellealw@gmail.com>
2019-10-19 00:05:48 -0500

    Added more tests for Navigation comp.

--- src/shared/components/__tests__/Navigation.test.js
+++ src/shared/components/__tests__/Navigation.test.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-import { cleanup, renderWithRouter, waitForElement } from 'utils/test-utils'
+import { cleanup, renderWithRouter } from 'utils/test-utils'
+import { Context as ResponsiveContext } from 'react-responsive'
 
-import App from '../../../screens/App'
+import App from 'screens/App'
+import { fireEvent, within } from '@testing-library/dom'
 
 afterEach(cleanup)
 
@@ -25,4 +27,35 @@ test('full app rendering/navigating', async () => {
   await navigate('/signup')
   const signupPageTitle = await findByTestId('signup-page-title')
   expect(signupPageTitle).toHaveTextContent('Sign Up')
+})
+
+test('clicking on nav links navigates to respective page', async () => {
+  const { getByTestId, findByTestId } = renderWithRouter(
+    <ResponsiveContext.Provider value={{ width: 1000 }}>
+      <App />
+    </ResponsiveContext.Provider>,
+  )
+
+  const { getByText } = within(getByTestId('navigation'))
+
+  // Click on Sign Up Nav Link
+  fireEvent.click(getByText(/sign up/i))
+  const signupPageTitle = await findByTestId('signup-page-title')
+  expect(signupPageTitle).toHaveTextContent('Sign Up')
+
+  // Click on Profile Nav Link
+  fireEvent.click(getByText(/profile/i))
+  const profilePageTitle = await findByTestId('profile-page-title')
+  expect(profilePageTitle).toHaveTextContent('Your Decks')
+})
+
+test('menu icon appears when navigation is under 599px', () => {
+  const { getByLabelText } = renderWithRouter(
+    <ResponsiveContext.Provider value={{ width: 400 }}>
+      <App />
+    </ResponsiveContext.Provider>,
+  )
+
+  const menuIcon = getByLabelText('menu')
+  expect(menuIcon).toBeInTheDocument();
 })
