b36963561fdda9b2cbcf3ada5896aee0c8786994
Miguel <miguellealw@gmail.com>
2019-10-20 00:46:03 -0500

    Made Responsive component correspond with theme breakpoints

--- src/shared/components/Responsive.js
+++ src/shared/components/Responsive.js
@@ -1,30 +1,35 @@
 import { useMediaQuery } from 'react-responsive'
+import theme from '../theme'
 
 // Desktop
 export const DesktopUp = ({ children }) => {
-  const isDesktop = useMediaQuery({ minWidth: 1200 })
+  const isDesktop = useMediaQuery({ minWidth: theme.breakpoints.desktopUp })
   return isDesktop ? children : null
 }
 
 export const BigDesktopUp = ({ children }) => {
-  const isDesktop = useMediaQuery({ minWidth: 1800 })
+  const isDesktop = useMediaQuery({ minWidth: theme.breakpoints.bigDesktopUp })
   return isDesktop ? children : null
 }
 
 // Tablet
 export const TabletPortraitUp = ({ children }) => {
-  const isTablet = useMediaQuery({ minWidth: 600 })
+  const isTablet = useMediaQuery({
+    minWidth: theme.breakpoints.tabletPortraitUp,
+  })
   return isTablet ? children : null
 }
 
 export const TabletLandscapeUp = ({ children }) => {
-  const isTablet = useMediaQuery({ minWidth: 900 })
+  const isTablet = useMediaQuery({
+    minWidth: theme.breakpoints.tabletLandscapeUp,
+  })
   return isTablet ? children : null
 }
 
 // Phone
 export const PhoneOnly = ({ children }) => {
-  const isMobile = useMediaQuery({ maxWidth: 599 })
+  const isMobile = useMediaQuery({ maxWidth: theme.breakpoints.phoneOnly })
   return isMobile ? children : null
 }
 
