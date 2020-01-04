26dfb1ff46a450f529f955ddf77756f7285dfb15
Miguel <miguellealw@gmail.com>
2020-01-04 01:09:21 -0600

    Minor style chagnes

--- src/screens/App/screens/Profile/FormContainer.js
+++ src/screens/App/screens/Profile/FormContainer.js
@@ -14,14 +14,6 @@ export default styled.form`
   margin: 0 auto;
   /* background: pink; */
 
-  h2 {
-    font-size: 3em;
-    font-weight: bold;
-    text-align: center;
-    color: ${props => props.theme.colors.primaryGray};
-    margin: 1em 0;
-  }
-
   ${({ theme }) => theme.media.phoneOnly`
     font-size: .6em;
     // width: 20em;
