756b980feb20ebe9cd9e7d8bbc8655b02afe4bbf
Miguel <miguellealw@gmail.com>
2020-01-04 01:02:36 -0600

    Some changes to yup auth schema

--- src/shared/yup-schemas/user-auth.js
+++ src/shared/yup-schemas/user-auth.js
@@ -2,60 +2,55 @@ import * as yup from 'yup'
 
 export const emailNotLongEnough = 'Email must be at least 3 characters'
 export const passwordNotLongEnough = 'Password must be at least 3 characters'
-export const invalidEmail = 'Email must be a valid email'
+export const invalidEmail = 'Email Must be Valid'
 
 export const registerPasswordValidation = yup
   .string()
   .min(3, passwordNotLongEnough)
   .max(255)
-  .required()
+  .required('Password is Required')
 
-export const validUserSchema = yup.object().shape({
+export const validLoginUserSchema = yup.object().shape({
   email: yup
     .string()
     .min(3, emailNotLongEnough)
     .max(255)
     .email(invalidEmail)
-    .required(),
+    .required('Email is Required'),
   password: registerPasswordValidation,
 })
 
-const invalidLogin = 'invalid login'
-
-export const loginSchema = yup.object().shape({
+export const validSignupUserSchema = yup.object().shape({
   email: yup
-    .string()
-    .min(3, invalidLogin)
-    .max(255, invalidLogin)
-    .email(invalidLogin)
-    .required("Email is Required"),
-  password: yup
-    .string()
-    .min(3, invalidLogin)
-    .max(255, invalidLogin)
-    .required("Password is Required"),
-})
-
-export const signupSchema = yup.object().shape({
-  email: yup
-    .string()
-    .min(3, invalidLogin)
-    .max(255, invalidLogin)
-    .email(invalidLogin)
-    .required('Email is required'),
+  .string()
+    .min(3, emailNotLongEnough)
+    .max(255)
+    .email(invalidEmail)
+    .required('Email is Required'),
   firstName: yup.string().required('First name is required'),
   lastName: yup.string().required('Last name is required'),
-  password: yup
-    .string()
-    .min(3, invalidLogin)
-    .max(255, invalidLogin)
-    .required('Password is Required'),
+  password: registerPasswordValidation,
   confirmPassword: yup
-    .string()
-    .oneOf([yup.ref('password')], 'Passwords do not match')
-    .required('Password Confirmation is Required'),
+  .string()
+  .oneOf([yup.ref('password')], 'Passwords do not match')
+  .required('Password Confirmation is Required'),
 })
 
-export const changePasswordSchema = yup.object().shape({
-  newPassword: registerPasswordValidation,
-})
+// const invalidLogin = 'Invalid Login'
+
+// export const loginSchema = yup.object().shape({
+//   email: yup
+//     .string()
+//     .min(3, invalidLogin)
+//     .max(255, invalidLogin)
+//     .email(invalidLogin)
+//     .required('Email is Required'),
+//   password: yup
+//     .string()
+//     .min(3, invalidLogin)
+//     .max(255, invalidLogin)
+//     .required('Password is Required'),
+// })
+// export const changePasswordSchema = yup.object().shape({
+//   newPassword: registerPasswordValidation,
+// })
