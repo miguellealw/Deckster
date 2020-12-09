b60a2a3faac2e4ed67a08d186650a02ee4d9f4f2
Miguel <miguellealw@gmail.com>
2020-12-08 20:00:31 -0600

    merging

--- src/screens/App/screens/Auth/AuthFormErrorMessage.js
+++ src/screens/App/screens/Auth/AuthFormErrorMessage.js
@@ -4,5 +4,5 @@ import FormErrorMessage from 'shared/components/FormErrorMessage'
 export default styled(FormErrorMessage)`
   margin-top: -1em;
   margin-bottom: 1em;
-  font-size: 0.8rem;
+  font-size: .8rem;
 `
